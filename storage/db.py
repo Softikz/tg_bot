c8dbc78284a2bd89d7b4b643695aa8da157ebb48
Unik <sergeymosin04@gmail.com>
2025-10-21 22:03:07 +0300

    Update db.py

--- storage/db.py
+++ storage/db.py
@@ -56,5 +56,35 @@ class DB:
         rows = self.cur.fetchall()
         return [dict(row) for row in rows]
 
+    # --- ↓↓↓ Добавленные методы из твоего кода ↓↓↓ ---
+
+    def reset_user_progress(self, user_id: int):
+        """Сбрасывает баланс и улучшения пользователя."""
+        self.cur.execute("""
+            UPDATE users
+            SET bananas=0,
+                per_click=1,
+                per_second=0,
+                upgrades='{}'
+            WHERE user_id=?
+        """, (user_id,))
+        self.conn.commit()
+
+    def add_gold_banana(self, user_id: int, amount: int = 1):
+        """Добавляет золотой банан пользователю."""
+        user = self.get_user(user_id)
+        gold_expires = max(time.time(), user.get("gold_expires", 0)) + 86400  # 1 день
+        self.update_user(user_id, gold_expires=gold_expires)
+
+    def add_passive_clicks(self, user_id: int, amount: int = 2):
+        """Добавляет пассив к пользователю."""
+        user = self.get_user(user_id)
+        self.update_user(user_id, per_second=user.get("per_second", 0) + amount)
+
+    def add_bananas(self, user_id: int, amount: int):
+        """Накрутка бананов (для себя)."""
+        user = self.get_user(user_id)
+        self.update_user(user_id, bananas=user.get("bananas", 0) + amount)
+
     def close(self):
         self.conn.close()
